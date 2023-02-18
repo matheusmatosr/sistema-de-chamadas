3c5eb27c023d0521def97245cac43f96f096bf3b
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2023-02-18 11:33:39 -0300

    first

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,9 @@
+function SignIn() {
+    return (
+      <div className="App">
+        <h1>Tela de login</h1>
+      </div>
+    );
+}
+  
+export default SignIn;
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import { Switch } from 'react-router-dom';
+import { Route } from './Route';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+export default function Routes(){
+    return(
+        <Switch>
+            <Route exact path="/" component={SignIn}/>
+            <Route exact path="/register" component={SignUp}/>
+        </Switch>
+    )
+}
