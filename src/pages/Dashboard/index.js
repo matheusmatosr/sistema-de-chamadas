efd0f33c7fb69c917f4bf265120117a1a8d13937
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2023-03-04 10:52:47 -0300

    update
    update

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import { AuthContext } from '../../contents/auth';
 import Header from "../../components/Header";
 import Title from '../../components/Title';
-import { FiDatabase } from 'react-icons/fi';
+import { FiDatabase, FiEdit2, FiPlus, FiSearch } from 'react-icons/fi';
+import { Link } from "react-router-dom";
+import './index.css';
 
 function Dashboard() {
   const { signOut } = useContext(AuthContext);
@@ -14,8 +16,64 @@ function Dashboard() {
         <Title name="Dashboard">
           <FiDatabase size={25} />
         </Title>
+
+        <>
+          <Link to="/new" className="new">
+            <FiPlus color="#fff" size={25} />
+            Novo chamado
+          </Link>
+
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Assunto</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastrado</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Cliente">Mercado esquina</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{backgroundColor: '#999'}} >
+                    Em aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado">04/03/2020</td>
+                <td data-label="#"></td>
+                  <button className="action" style={{backgroundColor: '#3583f6'}}>
+                    <FiSearch color="#fff" size={17} />
+                  </button>
+                  <button className="action" style={{backgroundColor: '#f6a935'}}>
+                    <FiEdit2 color="#fff" size={17} />
+                  </button>
+              </tr>
+
+
+              <tr>
+                <td data-label="Cliente">Pousada Lr</td>
+                <td data-label="Assunto">Hotelaria</td>
+                <td data-label="Status">
+                  <span className="badge" style={{backgroundColor: '#999'}} >
+                    Em aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado">04/03/2020</td>
+                <td data-label="#"></td>
+                  <button className="action" style={{backgroundColor: '#3583f6'}}>
+                    <FiSearch color="#fff" size={17} />
+                  </button>
+                  <button className="action" style={{backgroundColor: '#f6a935'}}>
+                    <FiEdit2 color="#fff" size={17} />
+                  </button>
+              </tr>
+            </tbody>
+          </table>
+        </>
       </div>
-      <button onClick={() => signOut() }>Logout</button>
     </div>
   );
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,43 @@
 import './index.css';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { FiSettings } from 'react-icons/fi';
+import { FiSettings, FiUpload } from 'react-icons/fi';
+import avatar from '../../assets/avatar.png';
+import './index.css';
 
 export default function Profile(){
+   
     return(
         <div>
             <Header />
             <div className='content'>
-                <Title name="Meu perfil">
+                <Title name="Minha conta">
                     <FiSettings size={25} />
                 </Title>
+
+                <div className='container'>
+                    <form className='form-profile'>
+                        <label className="label-avatar">
+                            <span>
+                                <FiUpload color='#fff' size={15} />
+                            </span>
+
+                            <input type="file" accept="image/*" /> <br />
+                            <img src={avatar} alt="foto de perfil" width={200} height={200} />
+                        </label>
+
+                        <label>Nome</label>
+                        <input type="text" placeholder='Seu nome' />
+
+                        <label>Email</label>
+                        <input type="text" placeholder='teste@teste.com' disabled={false}/>
+
+                        <button type='submit'>Salvar</button>
+                    </form>
+                </div>
+                <div className='container'>
+                    <button className='logout-btn'>Sair</button>
+                </div>
             </div>
         </div>
     )
